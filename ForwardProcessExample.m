41d8b8b1ad0d853e4115645eb747afbe3c72cab5
KosVla <32097629+KosVla@users.noreply.github.com>
2022-11-29 17:00:11 +0100

    Add files via upload

---
+++ ForwardProcessExample.m
@@ -0,0 +1,47 @@
+close ALL, clear, clc
+
+%% Define input parameter
+
+% Filtering parameters
+theta(1) = 39.7;    % omega_0 or omega_mid
+theta(2) = 4.68;    % omega_n or omega_dot
+theta(3) = 0.3;     % zeta_f
+
+% Modulation function parameters
+theta(4) = 0.0004;  % T0
+theta(5) = 12.2;    % T1
+theta(6) = 12.2;    % T2
+theta(7) = 0.413;   % alpha
+theta(8) = 0.552;   % beta
+theta(9) = 0.0744;  % sigma_max
+
+Ts = 0.02;          %Timestep
+N = 2000;           %Number of points 
+T = Ts*(0:N-1);
+SimulatedAccelerograms = 100;      %Number of simulated samples
+
+%Initialization to store properties of accelerograms (Peak Ground Acceleration etc.)
+[PGAs,RMSAs] = deal(zeros(SimulatedAccelerograms,1));
+[NCEs,ERMSAs] = deal(zeros(N,SimulatedAccelerograms));
+SyntheticEarthsRepo = zeros(SimulatedAccelerograms,N);
+
+for i=1:SimulatedAccelerograms
+    %Set random seed
+    RandStream.setGlobalStream(RandStream('mt19937ar','seed',i));
+    %Evaluate noise
+    u = randn(N,1);
+    %Forward process
+    tic
+    [z,x,q,h] = syntheticEQ(theta,u,Ts,0.25);
+    SyntheticEarthsRepo(i,:)=z;
+    toc
+    %Store properies
+    [PGAs(i),RMSAs(i),NCEs(:,i),ERMSAs(:,i), ZCs(:,i)] = EQcrit(z);
+end
+
+%Plot last sample
+figure(1)
+plot(T,z,'r')
+xlabel('Time (s)')
+ylabel('Acceleration (g)')
+
